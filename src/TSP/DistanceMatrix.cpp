08b22d2bbdd4d8519b5f5a5cec5360f010577dcd
Anita Pervin <anitapervin868@gmail.com>
2025-12-03 21:13:51 +0600

    Edit korlam xyz file

--- src/TSP/DistanceMatrix.cpp
+++ src/TSP/DistanceMatrix.cpp
@@ -2,10 +2,14 @@
 #include <iostream>
 using namespace std;
 
-
 string intToString(int n) {
-    if (n == 0) return "0";
+
+    if (n == 0) {
+
+        return "0";
+    }
     string s = "";
+
     bool neg = false;
 
     if (n < 0) {
@@ -20,89 +24,129 @@ string intToString(int n) {
 
     if (neg) s = "-" + s;
     return s;
- }
-
+}
 
  string doubleTo2Decimal(double x) {
     int intPart = (int)x;
+
     double frac = x - intPart;
 
-    int twoDigit = (int)(frac * 100 + 0.5); 
+    int twoDigit = (int)(frac * 100 + 0.5);
 
-    return intToString(intPart) + "." + (twoDigit < 10 ? "0" : "") + intToString(twoDigit);
+    string result = intToString(intPart) + ".";
+
+    if (twoDigit < 10) {
+        result += "0";
+    }
+
+    result += intToString(twoDigit);
+
+    return result;
  }
-
 
  string pad8(string s) {
-    while (s.length() < 8) s = " " + s;
-    return s;
+
+    while (s.length() < 8){
+
+        s = " " + s;
+     }
+        return s;
  }
 
- DistanceMatrix::DistanceMatrix(const vector<City>& cities) {
-    numCities = cities.size();
-    matrix.resize(numCities, vector<double>(numCities, 0.0));
+ DistanceMatrix::DistanceMatrix(const City* cities, int n) {
+    numCities = n;
+
+    matrix = new double*[numCities];
+
+    for (int i = 0; i < numCities; i++) {
+
+         matrix[i] = new double[numCities];
+    }
 
     for (int i = 0; i < numCities; i++) {
         for (int j = 0; j < numCities; j++) {
-            if (i == j) matrix[i][j] = 0.0;
-            else matrix[i][j] = cities[i].distanceTo(cities[j]);
-        }
+
+            if (i == j){
+
+                matrix[i][j] = 0.0;
+            }
+
+            else{
+
+                matrix[i][j] = cities[i].distanceTo(cities[j]);
+         }
+      }
     }
  }
 
- double DistanceMatrix::getDistance(int cityIndex1, int cityIndex2) const {
+double DistanceMatrix::getDistance(int cityIndex1, int cityIndex2) const {
 
-    if (cityIndex1 < 0 || cityIndex1 >= numCities ||
-        cityIndex2 < 0 || cityIndex2 >= numCities) {
+    if (cityIndex1 < 0 || cityIndex1 >= numCities || cityIndex2 < 0 || cityIndex2 >= numCities) {
 
-        cout << "Error: Invalid city index!" << endl;
-        return -1.0;
+            cout << "Invalid city index!" << endl;
+            return -1.0;
+    }
+            return matrix[cityIndex1][cityIndex2];
+  }
+
+     int DistanceMatrix::getNumCities() const {
+
+      return numCities;
     }
 
-    return matrix[cityIndex1][cityIndex2];
-}
+  double DistanceMatrix::calculateTourDistance(const int* tour, int tourSize) const {
 
-int DistanceMatrix::getNumCities() const {
+        double total = 0.0;
 
-    return numCities;
+    for (int i = 0; i < tourSize - 1; i++) {
+
+         total += getDistance(tour[i], tour[i + 1]);
+
+    }
+
+    if (tourSize > 1) {
+
+        total += getDistance(tour[tourSize - 1], tour[0]);
+    }
+
+       return total;
  }
 
- double DistanceMatrix::calculateTourDistance(const vector<int>& tour) const {
+  void DistanceMatrix::print() const {
 
-    double total = 0.0;
+       cout << "Distance Matrix (" << numCities << "x" << numCities << "):" << endl;
 
-    for (size_t i = 0; i < tour.size() - 1; i++)
-
-        total += getDistance(tour[i], tour[i + 1]);
-
-    if (tour.size() > 1)
-        total += getDistance(tour.back(), tour[0]);
-
-    return total;
- }
-
-void DistanceMatrix::print() const {
-
-    cout << "Distance Matrix (" << numCities << "x" << numCities << "):"<<endl;
-
-    cout << "        ";
+       cout << "        ";
 
     for (int i = 0; i < numCities; i++) {
 
         cout << pad8("City" + intToString(i));
-    }
+      }
 
-    cout << endl;
+        cout << endl;
 
     for (int i = 0; i < numCities; i++) {
 
         cout << "City" << i << " ";
 
         for (int j = 0; j < numCities; j++) {
-            
+
             string val = doubleTo2Decimal(matrix[i][j]);
-            cout << pad8(val);
+
+             cout << pad8(val);
         }
-        cout << "\n";
+
+        cout << endl;
     }
-}
+ }
+
+
+    DistanceMatrix::~DistanceMatrix() {
+
+    for (int i = 0; i < numCities; i++) {
+
+        delete[] matrix[i];
+    }
+
+    delete[] matrix;
+ }
