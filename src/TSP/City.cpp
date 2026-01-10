03a6f4e4c032fffec30ea7cc1f0860662d7f0650
Anita Pervin <anitapervin868@gmail.com>
2026-01-10 23:29:28 +0600

    update

--- src/TSP/City.cpp
+++ src/TSP/City.cpp
@@ -4,43 +4,49 @@
 #include <cmath>
 
 // default value
-City::City() : id(0), x(0.0), y(0.0) {}
+ City::City() : id(0), x(0.0), y(0.0) {}
 
 
-City::City(int idVal, double xVal, double yVal) 
+ City::City(int idVal, double xVal, double yVal) 
     : id(idVal), x(xVal), y(yVal) {}
 
     //City c(5, 10.5, 20.7);
 
 
 // getters
-int City::getId() const { 
-    return id; 
-}
+ int City::getId() const { 
 
-double City::getX() const { 
-    return x; 
-}
+      return id; 
+ }
 
-double City::getY() const { 
-    return y; 
-}
+ double City::getX() const { 
+
+     return x; 
+ }
+
+ double City::getY() const { 
+
+     return y; 
+ }
 
 // setters
-void City::setId(int idVal) { 
-    id = idVal; 
-}
+ void City::setId(int idVal) {
 
-void City::setX(double xVal) { 
-    x = xVal; 
-}
+     id = idVal; 
+ }
 
-void City::setY(double yVal) { 
-    y = yVal; 
-}
+  void City::setX(double xVal) { 
+
+     x = xVal; 
+ }
+
+ void City::setY(double yVal) { 
+
+     y = yVal; 
+ }
 
 // calculate Euclidean distance
-double City::distanceTo(const City& other) const {
+ double City::distanceTo(const City& other) const {
     double dx = x - other.x;
     double dy = y - other.y;
     return std::sqrt(dx * dx + dy * dy);
