8df7d6400ecd321a0b16aa6a3fe9af081670d796
Anita Pervin <anitapervin868@gmail.com>
2026-01-14 18:21:53 +0600

    Update

--- src/TSP/DistanceMatrix.h
+++ src/TSP/DistanceMatrix.h
@@ -16,22 +16,21 @@ public:
     DistanceMatrix(const City* cities,int n);
     ~DistanceMatrix();
     
-    // get distance between two cities by their indices
+    
 
     double getDistance(int cityIndex1, int cityIndex2) const;
     
-    // get total number of cities
+   
 
     int getNumCities() const;
     
-    // total distance for a tour
+    
 
     double calculateTourDistance(const int* tour,int tourSize) const;
     
-    // print the distance matrix 
-
+   
     void print() const;
 
-};
+ };
 
 #endif
